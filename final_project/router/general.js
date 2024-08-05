840c05147ff12193571d0b4466425cf468ebd159
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-05 01:35:58 -0400

    general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,7 +11,7 @@ public_users.post('/register', (req, res) => {
   if (username && password) {
     if (isValid(username)) {
       users.push({ 'username': username, 'password': password });
-      return res.status(200).json({ message: 'User successfully registered. Now you can login' });
+      return res.status(200).json({ message: 'User successfully registered.' });
     } else {
       return res.status(404).json({ message: 'User already exists!' });
     }
