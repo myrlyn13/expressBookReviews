1a3f7217a9ae9cc66021e1f7006e5bb80ec0493f
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-05 01:40:08 -0400

    index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,51 +1,34 @@
-// Import necessary modules
-const express = require('express'); // Import Express framework
-const jwt = require('jsonwebtoken'); // Import JSON Web Token for authentication
-const session = require('express-session') // Import Express session for session management
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const general_routes = require('./router/general.js').general;
 
-// Import route handlers
-const customer_routes = require('./router/auth_users.js').authenticated; // Import customer routes with authentication
-const genl_routes = require('./router/general.js').general; // Import general routes
-
-// Create an Express application instance
 const app = express();
 
-// Middleware to parse incoming JSON requests
 app.use(express.json());
 
-// Middleware to initalize session for routes under "/customer" path
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use('/customer', session({ secret: 'fingerprint_customer', resave: true, saveUninitialized: true }))
 
-// Middleware for authentication for routes under "/customer/auth" path
-app.use("/customer/auth/*", function auth(req,res,next){
- // Get the JWT token from the request header or query parameter
- const token = req.headers.authorization; // Assuming token is passed in the Authorization header
-
- // Check if token is provided
- if (!token) {
-  return res.status(401).json({ message: "Unauthorized: Token is missing" });
- }
-
- try }
-        //Verify the token 
-    const verified = jwt.verify(token, "fingerprint_customer"); // Replace "your_secret_key" with your actual secret key
-
-// Token is valid, proceed to the next middleware or route handler
-    next();
-} catch (error) {
-   // Token is invalid or expired
-   return res.status(401).json({ message: "Unauthorized: Invalid token" });
-}
+app.use('/customer/auth/*', function auth(req, res, next) {
+  if (req.session.authorization) {
+    const token = req.session.authorization['accessToken'];
+    jwt.verify(token, 'access', (err, user) => {
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: 'User not authenticated' })
+      }
+    });
+  } else {
+    return res.status(403).json({ message: 'User not logged in' })
+  }
 });
 
-// Define the port number
-const PORT =5000;
+const PORT = 5000;
 
-// Mount customer routes under "/customer"
-app.use("/customer", customer_routes);
+app.use('/customer', customer_routes);
+app.use('/', general_routes);
 
-// Mount general routes under "/" path
-app.use("/", genl_routes);
-
-// Start the server and listen on the defined port 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log('Server is running'));
